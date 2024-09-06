1b5e492ea20bf5e130af3dd8e9ea9633195c83e0
Bright Edem Gawu <bright.gawu@amalitech.com>
2024-09-06 12:39:29 +0000

    Initial Commit

---
+++ TodoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoApp
+//
+//  Created by AMALITECH-PC-105889 on 9/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
